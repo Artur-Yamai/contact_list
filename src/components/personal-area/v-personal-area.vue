d9635427f22ecee745885370d682d2e8dfb519db
Yamik <wbf.robit@gmail.com>
2020-07-22 19:40:23 +0500

    компоненты для личого кабинета

---
+++ src/components/personal-area/v-personal-area.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="personal-area">
+
+    <header class="header">
+      <h1 class="header__title">Contact list</h1>
+    </header>
+    
+    <v-contact-list></v-contact-list>
+
+  </div>
+</template>
+
+<script>
+import vContactList from './v-contact-list'
+
+export default {
+  components: {
+    vContactList
+  }
+  
+}
+</script>
+
+<style lang="scss">
+
+.header{
+  padding: 24px;
+  background-color: rgb(37, 201, 255);
+
+  &__title {
+    text-align: center;
+    color: white;
+  }
+}
+  
+</style>
