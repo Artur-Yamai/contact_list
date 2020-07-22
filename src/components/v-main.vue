5316002676593c603e21c0f84e1974575f0911c0
Yamik <wbf.robit@gmail.com>
2020-07-22 19:44:18 +0500

    init

--- src/components/v-main.vue
+++ src/components/v-main.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
-    1
+    <h3 class="not-authorized" 
+        v-if="!validate"
+    >You are not logged in</h3>
+
+    <v-personal-area v-else></v-personal-area>
+
   </div>
 </template>
 
 <script>
+import vPersonalArea from './personal-area/v-personal-area'
+
+import {mapGetters} from 'vuex'
+
 export default {
+  components: {
+    vPersonalArea
+  },
+
+  computed: {
+    ...mapGetters(['validate'])
+  }
   
 }
 </script>
 
 <style lang="scss">
+.not-authorized {
+  margin-top: 100px;
+  text-align-last: center;
+}
   
 </style>
