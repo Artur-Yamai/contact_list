46f9178201910a2407ca96441db077e45dbf98b9
Yamik <wbf.robit@gmail.com>
2020-07-23 23:24:04 +0500

    компоненты для отображения списка контактов

---
+++ src/components/personal-area/v-search-contacts.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="search-contacts">
+    <input type="text"
+           class="search-contacts-input input-radius"
+           v-model="fieldValue"
+    >
+    <button class=" search-contacts-button btn btn__green"
+            @click.prevent="searchEnterValue"
+    >Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fieldValue: ''
+    }
+  },
+
+  methods: {
+    searchEnterValue() {
+      let users = this.$store.getters['searchContact'](this.fieldValue);
+
+      this.$emit('foundContacts', users)
+    }
+  }
+  
+}
+</script>
+
+<style lang="scss">
+  .search-contacts {
+    padding: 8px 16px;
+    background-color: hsl(195, 80%, 57%);
+
+    &__input {
+      width: 40%;
+    }
+
+    &__button {
+
+    }
+  }
+</style>
